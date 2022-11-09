a6ccabe3395ee3180f7b9db14246675eacf9f4a7
Sally Nader <sally.nader@vodafone.com>
2022-11-09 16:54:22 +0200

    Edits data name.

--- asg-elb/data.tf
+++ asg-elb/data.tf
@@ -1,9 +1,9 @@
 // Gets default vpc and subnets
-data "aws_subnet_ids" "default_subnets_ids" {
+data "aws_subnet_ids" "default_subnets" {
   vpc_id = "vpc-0d9cce260f6f48c31"
 }
 
 data "aws_subnet" "default_subnet" {
-  count = length(data.aws_subnet_ids.default_subnets_ids.ids)
-  id    = tolist(data.aws_subnet_ids.default_subnets_ids.ids)[count.index]
+  count = length(data.aws_subnet_ids.default_subnets.ids)
+  id    = tolist(data.aws_subnet_ids.default_subnets.ids)[count.index]
 }
