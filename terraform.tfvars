758a9175feddb4f61bf93a01475243a6551f8bf2
Sally Nader <sally.nader@vodafone.com>
2022-11-10 06:01:46 +0200

    Adds key pair to asg.

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,6 @@
 web_max_size = 3
 web_min_size = 1
+key_name = "ec2-ssh"
 alb_name = "web-alb"
 web_desired_capacity = 1
 target_group_name = "web-target-group"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
+variable "key_name"{}
+variable "alb_name" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "target_group_name" {}
-variable "alb_name" {}
 
 variable "region" {
   default = "us-east-1"
